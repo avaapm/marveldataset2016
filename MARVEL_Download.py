ca6c45583fac0a43db31763488b76ca123316e53
avaapm <aselsanavaapm@gmail.com>
2016-09-28 14:49:44 +0300

    Add files via upload

---
+++ MARVEL_Download.py
@@ -0,0 +1,202 @@
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+from PIL import Image
+import traceback
+import threading
+import datetime
+import logging
+import codecs
+import math
+import sys
+import os
+
+##Uncomment the related dat file ('VesselClassification.dat' for Vessel Classification, 'IMOTrainAndTest.dat' for Vessel Verification/Retrieval/Recognition tasks.)
+FILE_TO_DOWNLOAD_FROM = "VesselClassification.dat"
+##FILE_TO_DOWNLOAD_FROM = "IMOTrainAndTest.dat" 
+
+NUMBER_OF_WORKERS = 10
+MAX_NUM_OF_FILES_IN_FOLDER = 5000
+IMAGE_HEIGHT = 256
+IMAGE_WIDTH = 256
+ORIGINAL_SIZE = 0 # 1 for yes, 0 for no
+JUST_IMAGE = 1 # 1 for yes, 0 for no
+
+
+photoDetails = ["Photographer:","Title:","Captured:","IMO:","Photo Category:","Description:"]
+vesselIdentification = ["Name:","IMO:","Flag:","MMSI:","Callsign:"]
+technicalData = ["Vessel type:","Gross tonnage:","Summer DWT:","Length:","Beam:","Draught:"]
+additionalInformation = ["Home port:","Class society:","Build year:","Builder (*):","Owner:","Manager:"]
+aisInformation = ["Last known position:","Status:","Speed, course (heading):","Destination:","Last update:","Source:"]
+impText = photoDetails + vesselIdentification + technicalData + additionalInformation  
+impText2 = ["Former name(s):"]
+
+sourceLink = "http://www.shipspotting.com/gallery/photo.php?lid="
+
+logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-10s) %(message)s', )
+logging.debug("Process started at " + str(datetime.datetime.now()))
+
+def save_image(ID,justImage,outFolder):
+    url = sourceLink + ID
+    html = urlopen(url,timeout = 300).read()
+    soup = BeautifulSoup(html,"lxml")
+
+    images = [img for img in soup.findAll('img')]
+    image_links = [each.get('src') for each in images]
+    if not justImage:
+        tags = [tr for tr in soup.findAll('td')]
+        tr_text = [each.getText() for each in tags]
+        
+    filename = " "
+    for each in image_links:
+        if "http" in each and "jpg" in each and "photos/middle" in each:
+            filename=each.split('/')[-1]
+            f = urlopen(each)
+            with open(os.path.join(outFolder,filename), "wb") as local_file:
+                local_file.write(f.read())
+            if ORIGINAL_SIZE == 0:
+                img = Image.open(os.path.join(outFolder,filename)).resize((IMAGE_HEIGHT,IMAGE_WIDTH), Image.ANTIALIAS)
+                os.remove(os.path.join(outFolder,filename))
+                out = file(os.path.join(outFolder,filename),"wb")
+                img.save(out,"JPEG")
+            break
+        
+    if filename != " " and not justImage:
+        textFile = filename.split('.')[0]
+        tFile = codecs.open(os.path.join(outFolder,filename)+'.dat','w','utf-8')    
+        for index,each in enumerate(tr_text):
+            for impT in impText:
+                if impT == each:
+                    tFile.write(each + ' ' + tr_text[index+1] + '\n')
+                    break
+        for index,each in enumerate(tr_text):
+            for impT in impText2:
+                if impT == each:
+                    for ind in range(1,20):
+                        if tr_text[index+ind] != "":
+                            tFile.write(each + ' ' + tr_text[index+ind] + '\n')
+                        else:
+                            break
+                    break
+        tFile.close()
+    if filename == " ":
+        return 0
+    else:
+        return 1
+
+
+def worker(content,workerNo):
+    workerIndex = 0
+    folderIndex = 0
+    folderNo = 1
+    currFolder = os.path.join(os.getcwd(),'W'+str(workerNo)+'_'+str(folderNo))
+    if not os.path.exists(currFolder):
+        os.mkdir(currFolder)
+    for ID in content:
+        if folderIndex == MAX_NUM_OF_FILES_IN_FOLDER:
+            folderIndex = 0
+            folderNo = folderNo + 1
+            currFolder = os.path.join(os.getcwd(),'W'+str(workerNo)+'_'+str(folderNo))
+            if not os.path.exists(currFolder):
+                os.mkdir(currFolder)
+        try:
+            status = save_image(ID,JUST_IMAGE,currFolder)
+            workerIndex = workerIndex + 1
+            if status == 1:
+                folderIndex = folderIndex + 1
+                logging.debug(str(ID) + "\t - Downloaded... - " + str(workerIndex) + "\t/" + str(len(content)))
+            else:
+                logging.debug(str(ID) + "\t - NO SUCH FILE  - " + str(workerIndex) + "\t/" + str(len(content)))
+        except:
+            traceback.print_exc()
+    logging.debug(str(datetime.datetime.now()) + "-------------- DONE ")
+    return
+
+priorFiles = []
+dirs = os.listdir(os.getcwd())
+for eachDir in dirs:
+    if 'W' in eachDir:
+        oldFiles = os.listdir(os.path.join(os.getcwd(),eachDir))
+        for eachFile in oldFiles:
+            if ".jpg" in eachFile:
+                oldID = eachFile.split(".")[0]
+                priorFiles.append(oldID)
+
+downloadFile = codecs.open(FILE_TO_DOWNLOAD_FROM,"r","utf-8")
+downloadContent = downloadFile.readlines()
+downloadFile.close()
+finalContent = []
+for index,eachLine in enumerate(downloadContent):
+    temp = eachLine.split(',')[0]
+    if temp not in priorFiles:
+        finalContent.append(temp)
+
+numOfFiles = len(finalContent)
+
+numOfFilesPerEachWorker = [int(math.floor(float(numOfFiles)/NUMBER_OF_WORKERS)) for x in range(0,NUMBER_OF_WORKERS-1)]
+numOfFilesPerEachWorker.append(numOfFiles - (NUMBER_OF_WORKERS-1)*int(round(numOfFiles/NUMBER_OF_WORKERS,0)))
+
+logging.debug("There will be %s workers in this download process" % NUMBER_OF_WORKERS)
+logging.debug("%s files will be downloaded" % numOfFiles)
+
+threads = []
+imageCount = 0
+for i in range(0,NUMBER_OF_WORKERS):
+    t = threading.Thread(name='Worker'+str(i), target=worker, args=(finalContent[imageCount:imageCount + numOfFilesPerEachWorker[i]],i,))
+    imageCount = imageCount + numOfFilesPerEachWorker[i]
+    threads.append(t)
+    t.start()
+
+flag = True
+while flag:
+    counter = 0
+    for eachT in threads:
+        if eachT.isAlive() == False:
+            counter = counter + 1
+    if counter == NUMBER_OF_WORKERS:
+        flag = False
+
+logging.debug(str(datetime.datetime.now()) + " - list all files startes ")
+allPaths = []
+allIDs = []
+dirs = os.listdir(os.getcwd())
+for eachDir in dirs:
+    if 'W' in eachDir:
+        FinalList = os.listdir(os.path.join(os.getcwd(),eachDir))
+        for eachFile in FinalList:
+            if ".jpg" in eachFile:
+                fPath = os.path.join(os.getcwd(),eachDir,eachFile)
+                fID = eachFile.split(".")[0]
+                allPaths.append(fPath)
+                allIDs.append(fID)
+logging.debug(str(datetime.datetime.now()) + " - write to disc ")
+
+FINAL = codecs.open("FINAL.dat","w","utf-8")
+for eachLine in downloadContent:
+    tempID = eachLine.split(",")[0]
+    try:
+        tempIndex = allIDs.index(tempID)
+        FINAL.write(eachLine[:-1]+","+str(allPaths[tempIndex])+"\n")
+    except:
+        FINAL.write(eachLine[:-1]+","+"-\n")
+FINAL.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                  
+
+
+
+
+
---
+++ ReadMe.txt
@@ -0,0 +1,97 @@
+ Author: Erhan Gundogdu
+ Last Update: 09/28/2016
+
+ CITATION:
+ If you use MARVEL in your research, please cite:
+	@PROCEEDINGS {MARVEL,
+    author       = "Gundogdu E., Solmaz B, Yucesoy V., Koc A.",
+    title        = "Marvel: A Large-Scale Image Dataset for Maritime Vessels",
+    year         = "2016",
+    organization = "Asian Conference on Computer Vision (ACCV)"
+	}
+ 
+ This document is to explain the use of MARVEL and its metadata and to download it from the web.
+ 
+ (0) Downloading MARVEL dataset:
+	In order to download the MARVEL dataset, 'MARVEL_Download.py' Python script must be run with the appropriate
+	.dat file, i.e. Uncomment the related dat file: 'VesselClassification.dat' for Vessel Classification,
+	'IMOTrainAndTest.dat' for Vessel Verification/Retrieval/Recognition tasks.	
+	Default values and explanation of the download parameters inside MARVEL_Download.py are:
+		NUMBER_OF_WORKERS = 10 ## Number of threads that will be working in parallel
+		MAX_NUM_OF_FILES_IN_FOLDER = 5000 ## Maximum number of files in a folder
+		IMAGE_HEIGHT = 256 ## image height unless ORIGINAL_SIZE = 1
+		IMAGE_WIDTH = 256 ## image width unless ORIGINAL_SIZE = 1
+		ORIGINAL_SIZE = 0 # 1 for yes, 0 for no
+		JUST_IMAGE = 1 # 1 for yes, 0 for no
+	
+	The images are downloaded to the location of MARVEL_Download.py with a meta folder structure.
+	Once the download is finished, the vessel images are distributed in a meta folder structure.
+	The output filename is FINAL.dat, which has the same structure as the input XXX.dat file except for appended path names
+	as the last column of each line.
+	
+	e.g. one line from input IMOTrainAndTest.dat file: '993293,1,1,Container Ship'
+	and one line from output FINAL.dat file: '993293,1,1,Container Ship,path\to\your\folder\W1_1\993293.jpg'
+	
+	
+ (1) Vessel Classification:
+	VesselClassification.dat is the corresponding file for vessel classification.
+	
+	Each line carries the information related to the training and the test images in the following structure:
+		vessel ID, set index, class label, class label name
+	vessel ID: is the ID consistent with the url name in the corresponding website and helps us to download the image.
+	set index: 1 or 2 (1 for training and 2 for test)
+	class label: vessel type label enumeration (from 1 to 26)
+	class label name: the name of the class label given in our paper
+	
+	
+ (2) IMO Training and Test Sets:
+	IMOTrainAndTest.dat is the corresponding file containing information of IMO train and test sets defined in the paper.
+	
+	Each line carries the information related to the IMO train and the test images in the following structure:
+			vessel ID, set index, class label, class label name, IMO Number
+		vessel ID: is the ID consistent with the url name in the corresponding website and helps us to download the image.
+		set index: 1 or 2 (1 for training and 2 for test)
+		class label: vessel type label enumeration (from 1 to 26)
+		class label name: the name of the class label given in our paper
+		IMO number: is International Maritime Organization number of the corresponding vessel taken from the website
+	Note: The IMO Train and test sets contain 400K images in total and Vessel Verification/Retrieval/Recognition set indices
+	are based on the ordering of these 400K image list.
+		
+ (3) Vessel Verification:
+	VesselVerificationTest.dat and VesselVerificationTrain.dat are the vessel verification task related .dat files.
+	In each file, every line corresponds to the indices of the pairs of images for positive and negative examples.
+	As stated in the paper, a positive example is a pair of images belonging to the same vessel
+	and a negative example is a pair of images belonging to different vessels. These pairs are randomly sampled from
+	the IMO train and test sets.
+	
+	Each line has the following structure:
+			vessel index 1, vessel index 2, set index
+		vessel index 1/2: the index of the vessels within a pos/neg example with respect to the IMO train/test set.
+		set index: 0 or 1 (0 for negative examples, 1 for positive examples)
+	
+	Note: In order to use the VesselVerificationTest.dat and VesselVerificationTrain.dat files, IMOTrainAndTest.dat 
+	is required, since the indices of this task are given with respect to the 400K IMO training and test sets.
+ (4) Vessel Retrieval
+	ChiTest.dat, ChiTrain.dat, EucTest.dat, EucTrain.dat are the corresponding meta files for vessel retrieval tasks for
+	the chi square and euclidean distances.
+	In each file, every line corresponds to training and test examples.
+	
+	Each line has the following structure:
+			vessel index, class label, class name
+		vessel index: the index of the corresponding example with respect to the IMO train/test set.
+		class label: class label of the example out of 109 different vessels (as stated in the paper.)
+		class name: name of the vessel type (as stated in the paper)
+	Note: In order to use the VesselVerificationTest.dat and VesselVerificationTrain.dat files, IMOTrainAndTest.dat 
+	is required, since the indices of this task are given with respect to the 400K IMO training and test sets.
+
+ (5) Vessel Recognition
+	The folder with the name Recognition contains 29 classes for the purpose of recognizing individual vessel within their
+	corresponding vessel types. This folder contains seperate foldera for different 29 vessel types.
+	Each vessel type folder, e.g. Bulk Carrier, contain five different folds for training and testing. Each .dat file, i.e.
+	trainSplit_X, has the following structure:
+			vesselindex, IMO number
+		vessel index: the index of the corresponding example with respect to the IMO train/test set.
+		IMO number: IMO number for the example vessel image (This label is required for training a classifier and can also 
+		be obtained from the first column of IMOTrainAndTest.dat file with the help of the vessel index.)
+	Note: In order to use the VesselVerificationTest.dat and VesselVerificationTrain.dat files, IMOTrainAndTest.dat 
+	is required, since the indices of this task are given with respect to the 400K IMO training and test sets.
