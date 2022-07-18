3b0480bf77659b760155d9cb0995b5696f02b3f7
Tony Davis <Anthony.Davis@jhuapl.edu>
2022-07-18 14:32:13 -0400

    remove ,

--- MARVEL_Download.py
+++ MARVEL_Download.py
@@ -3,14 +3,16 @@ from urllib.request import urlopen, Request, build_opener, install_opener
 from PIL import Image
 import traceback
 import threading
-import datetime
-import logging
+# import datetime
+# import logging
 import codecs
 import math
 import os
 import ssl 
 ssl._create_default_https_context = ssl._create_unverified_context
-
+# import requests
+import time
+from tqdm import tqdm
 
 ##Uncomment the related dat file ('VesselClassification.dat' for Vessel Classification, 'IMOTrainAndTest.dat' for Vessel Verification/Retrieval/Recognition tasks.)
 # FILE_TO_DOWNLOAD_FROM = "VesselClassification.dat"
@@ -18,7 +20,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 ##FILE_TO_DOWNLOAD_FROM = "IMOTrainAndTest.dat" 
 crop = 20
 NUMBER_OF_WORKERS = 10
-MAX_NUM_OF_FILES_IN_FOLDER = 5000
+# MAX_NUM_OF_FILES_IN_FOLDER = 5000
 IMAGE_HEIGHT = 720
 IMAGE_WIDTH = 1280
 ORIGINAL_SIZE = 0 # 1 for yes, 0 for no
@@ -32,17 +34,35 @@ cats = [os.path.splitext(x)[0] for x in os.listdir(datadir)]
 # sourceLink = "http://www.shipspotting.com/gallery/photo.php?lid="
 sourceLink = "http://www.shipspotting.com/photos/"
 
-logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-10s) %(message)s', )
-logging.debug("Process started at " + str(datetime.datetime.now()))
+# logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-10s) %(message)s', )
+# logging.debug("Process started at " + str(datetime.datetime.now()))
+
+# headers = {
+#     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:96.0) Gecko/20100101 Firefox/96.0",
+# }
 
 def save_image(ID,justImage,outFolder):
     url = sourceLink + ID
     opener = build_opener()
-    opener.addheaders = [('User-Agent', 'MyApp/1.0')]
+    # opener.addheaders = [('User-Agent', 'MyApp/1.0')]
+    opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:96.0) Gecko/20100101 Firefox/96.0')]
     install_opener(opener)
     req = Request(url)
-    html = urlopen(req, timeout=300).read()
+    # html = requests.get(url, headers=headers).text
+    works = False
+    while not works:
+        wait = 1
+        try:
+            html = urlopen(req, timeout=300).read()
+            works = True
+        except:
+            traceback.print_exc()
+            print(f'Trying again after {wait} seconds')
+            time.sleep(wait)
+            wait = wait + 1
     soup = BeautifulSoup(html,"lxml")
+    # soup = BeautifulSoup(html,"html.parser")
+
 
     images = [img for img in soup.find_all('img')]
     image_links = [each.get('src') for each in images]
@@ -53,7 +73,17 @@ def save_image(ID,justImage,outFolder):
     for each in image_links:
         if "http" in each and "jpg" in each and "photos/big" in each:
             filename=each.split('/')[-1]
-            f = urlopen(each)
+            works = False
+            wait = 1
+            while not works:
+                try:
+                    f = urlopen(each)
+                    works = True
+                except:
+                    wait = wait + 1
+                    traceback.print_exc()
+                    print(f'Trying again after {wait} seconds')
+                    time.sleep(1)
             with open(os.path.join(outFolder,filename), "wb") as local_file:
                 local_file.write(f.read())
             if ORIGINAL_SIZE == 0:
@@ -81,10 +111,10 @@ def save_image(ID,justImage,outFolder):
         return 1
 
 
-def worker(content,workerNo,cat):
+def worker(content,cat):
     workerIndex = 0
     folderIndex = 0
-    folderNo = 1
+    # folderNo = 1
 
     # currFolder = os.path.join(os.getcwd(),'W'+str(workerNo)+'_'+str(folderNo))
     saveFolder = os.path.join(savedir, cat)
@@ -109,12 +139,12 @@ def worker(content,workerNo,cat):
             workerIndex = workerIndex + 1
             if status == 1:
                 folderIndex = folderIndex + 1
-                logging.debug(str(ID) + "\t - Downloaded... - " + str(workerIndex) + "\t/" + str(len(content)))
-            else:
-                logging.debug(str(ID) + "\t - NO SUCH FILE  - " + str(workerIndex) + "\t/" + str(len(content)))
+                # logging.debug(str(ID) + "\t - Downloaded... - " + str(workerIndex) + "\t/" + str(len(content)))
+            # else:
+                # logging.debug(str(ID) + "\t - NO SUCH FILE  - " + str(workerIndex) + "\t/" + str(len(content)))
         except:
             traceback.print_exc()
-    logging.debug(str(datetime.datetime.now()) + "-------------- DONE ")
+    # logging.debug(str(datetime.datetime.now()) + "-------------- DONE ")
     return
 
 def main(cat):
@@ -134,7 +164,7 @@ def main(cat):
     downloadContent = downloadFile.readlines()
     downloadFile.close()
     finalContent = []
-    for index,eachLine in enumerate(downloadContent):
+    for eachLine in downloadContent:
         temp = (eachLine.split(',')[0],eachLine.split(',')[2])
         if temp not in priorFiles:
             finalContent.append(temp)
@@ -144,40 +174,43 @@ def main(cat):
     numOfFilesPerEachWorker = [int(math.floor(float(numOfFiles)/NUMBER_OF_WORKERS)) for x in range(0,NUMBER_OF_WORKERS-1)]
     numOfFilesPerEachWorker.append(numOfFiles - (NUMBER_OF_WORKERS-1)*int(round(numOfFiles/NUMBER_OF_WORKERS,0)))
 
-    logging.debug("There will be %s workers in this download process" % NUMBER_OF_WORKERS)
-    logging.debug("%s files will be downloaded" % numOfFiles)
+    # logging.debug("There will be %s workers in this download process" % NUMBER_OF_WORKERS)
+    print(f'There will be {NUMBER_OF_WORKERS} workers in this download process')
+    # logging.debug("%s files will be downloaded" % numOfFiles)
+    print(f'{numOfFiles} files will be downloaded')
 
     threads = []
     imageCount = 0
     for i in range(0,NUMBER_OF_WORKERS):
-        t = threading.Thread(name='Worker'+str(i), target=worker, args=(finalContent[imageCount:imageCount + numOfFilesPerEachWorker[i]],i,cat))
+        t = threading.Thread(name='Worker'+str(i), target=worker, args=(finalContent[imageCount:imageCount + numOfFilesPerEachWorker[i]],cat))
         imageCount = imageCount + numOfFilesPerEachWorker[i]
         threads.append(t)
         t.start()
 
-    flag = True
-    while flag:
-        counter = 0
-        for eachT in threads:
-            if eachT.is_alive() == False:
-                counter = counter + 1
-        if counter == NUMBER_OF_WORKERS:
-            flag = False
+    # flag = True
+    # while flag:
+    #     counter = 0
+    #     for eachT in threads:
+    #         if eachT.is_alive() == False:
+    #             counter = counter + 1
+    #     if counter == NUMBER_OF_WORKERS:
+    #         flag = False
 
-    logging.debug(str(datetime.datetime.now()) + " - list all files startes ")
-    allPaths = []
-    allIDs = []
-    dirs = os.listdir(os.getcwd())
-    for eachDir in dirs:
-        if 'W' in eachDir:
-            FinalList = os.listdir(os.path.join(os.getcwd(),eachDir))
-            for eachFile in FinalList:
-                if ".jpg" in eachFile:
-                    fPath = os.path.join(os.getcwd(),eachDir,eachFile)
-                    fID = eachFile.split(".")[0]
-                    allPaths.append(fPath)
-                    allIDs.append(fID)
-    logging.debug(str(datetime.datetime.now()) + " - write to disc ")
+    # logging.debug(str(datetime.datetime.now()) + " - list all files startes ")
+
+    # allPaths = []
+    # allIDs = []
+    # dirs = os.listdir(os.getcwd())
+    # for eachDir in dirs:
+    #     if 'W' in eachDir:
+    #         FinalList = os.listdir(os.path.join(os.getcwd(),eachDir))
+    #         for eachFile in FinalList:
+    #             if ".jpg" in eachFile:
+    #                 fPath = os.path.join(os.getcwd(),eachDir,eachFile)
+    #                 fID = eachFile.split(".")[0]
+    #                 allPaths.append(fPath)
+    #                 allIDs.append(fID)
+    # logging.debug(str(datetime.datetime.now()) + " - write to disc ")
 
     # FINAL = codecs.open("FINAL.dat","w","utf-8")
     # for eachLine in downloadContent:
@@ -190,7 +223,10 @@ def main(cat):
     # FINAL.close()
 
 if __name__ == '__main__':
-    for cat in cats:
+    for cat in tqdm(cats):
+        if cat in os.listdir(savedir):
+            print(f'{cat} already downloaded')
+            continue
         print('Begin downloading ' + cat)
         main(cat)
 
--- remove_multi.py
+++ remove_multi.py
@@ -8,10 +8,13 @@ for cat in cats:
     print('Processing category: ' + cat)
     df = pd.read_csv('category_data/' + cat, header=None)
     col = df.iloc[:,2]
+    rmv_cma = col.str.replace(',', '') 
+    df.iloc[:,2] = rmv_cma
     rows_no_amp = col.loc[col.str.find('&') == -1]
     rows_no_dash = col.loc[col.str.find(' - ') == -1]
     rows_no_and = col.loc[col.str.find(' AND ') == -1]
     rows_no_plus = col.loc[col.str.find('+') == -1]
+    # also could remove all 0,0,0 rows
     rows_to_save = [list(rows_no_amp.index),list(rows_no_dash.index),list(rows_no_and.index),list(rows_no_plus.index)]
     rows_to_save = list(set.intersection(*map(set, rows_to_save)))
     df = df.iloc[rows_to_save,:]
